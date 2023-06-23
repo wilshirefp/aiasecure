ce1dc5633d5cce7cf7d4ec6e8b9e23aa71d09eff
Joe Vasquez <marketing@pantummedia.com>
2023-06-23 12:36:50 -0700

    updated image config deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images:{
-    domains: ["vercel.app","aiasecurities-o2e4rbc02-marketing-pantummedia.vercel.app"]
+    domains: ["vercel.app","aiasecurities-o2e4rbc02-marketing-pantummedia.vercel.app"],
+    unoptimized:  true,
   },
   loader:"imgix",
   path:"",
