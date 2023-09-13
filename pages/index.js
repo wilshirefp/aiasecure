e941d11ce279acba16f9d12d4a3cbaabd7512621
wilshirefp <143762557+wilshirefp@users.noreply.github.com>
2023-09-12 18:30:26 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,21 @@ import Insights from '../components/Insights'
 import Institutional from '../components/Institutional'
 import WhyAA from '../components/WhyAA'
 import StayConnected from '../components/StayConnected'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+<Script src="https://www.googletagmanager.com/gtag/js?id=js?id=G-9TVMWXLSDY" />
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
+          gtag('config', 'js?id=G-9TVMWXLSDY');
+        `}
+      </Script>
       <Head>
         <title>AIA Securities | Investment Banking</title>
         <meta name="description" content="AIA Securities is a nationwide, investment banking firm delivering a range of corporate finance services to investors and clients." />
